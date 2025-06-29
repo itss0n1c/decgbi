4bdb244ce34640c20f6e9447462b8a1ac82c93f3
S0n1c <itss0n1cdev@gmail.com>
2025-06-29 15:42:01 -0400

    fix: typescript types

--- build.ts
+++ build.ts
@@ -3,13 +3,13 @@ import { rm } from 'node:fs/promises';
 
 const clear_dist = () => rm('dist', { recursive: true, force: true });
 
-const build_ts = (is_browser: boolean) =>
+const build_ts = () =>
 	build({
 		minify: true,
-		target: is_browser ? 'browser' : 'node',
-		entrypoints: [`lib/${is_browser ? 'browser' : 'index'}.ts`],
+		target: 'node',
+		entrypoints: ['lib/index.ts'],
 		outdir: 'dist',
-		naming: `[dir]/${is_browser ? 'browser' : 'index'}.js`,
+		// naming: `[dir]/index.js',
 		packages: 'external',
 		sourcemap: 'linked',
 	});
@@ -17,14 +17,13 @@ const build_ts = (is_browser: boolean) =>
 const build_types = () => $`tsc -p .`;
 
 async function clean_types() {
-	const files = ['browser.d.ts', 'index.test.d.ts'];
+	const files = ['index.test.d.ts'];
 	for (const file of files) await rm(`dist/${file}`);
 }
 
 clear_dist()
 	.then(() => console.log('Cleared dist directory!'))
-	.then(() => build_ts(false))
-	.then(() => build_ts(true))
+	.then(() => build_ts())
 	.then(() => console.log('TypeScript build complete!'))
 	.then(build_types)
 	.then(clean_types)
